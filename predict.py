ca105227ad64f105887754e9377a117b1227d3ac
pattonw <pattonw@hhmi.org>
2022-03-02 13:37:41 -0800

    predict on directory

--- predict.py
+++ predict.py
@@ -20,8 +20,10 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.metrics import confusion_matrix, roc_auc_score, classification_report
 import math
 
+from pathlib import Path
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
+
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
 assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
 config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
@@ -37,21 +39,47 @@ config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
     "--csv", type=click.Path(exists=True), help="The data you want to predict on."
 )
 def predict(model, csv):
+    csv_file = csv
+    csv_path = Path(csv)
 
-    batch_size = 256
+    if csv_path.is_dir():
+        for csv_file in csv_path.iterdir():
+            batch_size = 256
 
-    csv = pd.read_csv(csv, header=None)
-    dataset = csv.values
-    dataset = dataset.astype('float64')
+            csv = pd.read_csv(csv, header=None)
+            dataset = csv.values
+            dataset = dataset.astype("float64")
 
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    dataset = scaler.fit_transform(dataset)
-    loaded_model = load_model(model)
+            scaler = MinMaxScaler(feature_range=(0, 1))
+            dataset = scaler.fit_transform(dataset)
+            loaded_model = load_model(model)
 
-    c = loaded_model.predict_classes(dataset[:,1].reshape((-1, 1, 1, 1)), batch_size=batch_size, verbose=True)
+            c = loaded_model.predict_classes(
+                dataset[:, 1].reshape((-1, 1, 1, 1)),
+                batch_size=batch_size,
+                verbose=True,
+            )
 
-    idealized = pd.DataFrame(c, index=dataset[:, 0])
-    idealized.to_csv(f"{csv[:-4]}_idealized.csv")
+            idealized = pd.DataFrame(c, index=dataset[:, 0])
+            idealized.to_csv(csv_path / f"{csv_file.name[:-4]}_idealized.csv")
+    else:
+        batch_size = 256
+
+        csv = pd.read_csv(csv, header=None)
+        dataset = csv.values
+        dataset = dataset.astype("float64")
+
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        dataset = scaler.fit_transform(dataset)
+        loaded_model = load_model(model)
+
+        c = loaded_model.predict_classes(
+            dataset[:, 1].reshape((-1, 1, 1, 1)), batch_size=batch_size, verbose=True
+        )
+
+        idealized = pd.DataFrame(c, index=dataset[:, 0])
+        idealized.to_csv(csv_path/f"{csv_file.name[:-4]}_idealized.csv")
+
 
 if __name__ == "__main__":
     predict()
