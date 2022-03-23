166125dc2fc9b2617b669f7bd3f59fe5978ed846
pattonw <pattonw@hhmi.org>
2022-03-23 12:42:41 -0700

    fix csv iteration

--- extract_events.py
+++ extract_events.py
@@ -23,13 +23,16 @@ def convert(csv, event_conductance_min, event_conductance_max):
     during_event = 0
     previous_value = None
     new_csv = []
-    for (time, value) in csv.iterrows():
+    for i, (indx, time, value) in csv.iterrows():
         change = 0 if previous_value is None else value - previous_value
         if during_event == 0 and event_conductance_min <= change and event_conductance_max >= change:
+            print(f"Starting event at time {time}")
             during_event = 1
         elif during_event == 1 and change < 0:
+            print(f"Ending event at time {time}")
             during_event = 0
         new_csv.append((time, during_event))
+        previous_value = value
 
     new_csv = pd.DataFrame(new_csv, columns=["time", "event"])
     new_csv.to_csv(csv_path.parent / f"{csv_path.name[:-4]}_events.csv")
