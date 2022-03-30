2ce7af0023eb8b961905d6d2e77e83bc0aa7b372
pattonw <pattonw@hhmi.org>
2022-03-30 11:14:45 -0700

    Extract events from left to right and right to left

--- extract_events.py
+++ extract_events.py
@@ -10,7 +10,9 @@ from copy import deepcopy
 
 
 @click.command()
-@click.option("--csv", type=click.Path(exists=True), help="The csv from which to extract events.")
+@click.option(
+    "--csv", type=click.Path(exists=True), help="The csv from which to extract events."
+)
 @click.option("--event-conductance-min", type=float)
 @click.option("--event-conductance-max", type=float)
 def convert(csv, event_conductance_min, event_conductance_max):
@@ -22,20 +24,44 @@ def convert(csv, event_conductance_min, event_conductance_max):
 
     during_event = 0
     previous_value = None
-    new_csv = []
+    forwards_csv = []
     for i, (indx, time, value) in csv.iterrows():
         change = 0 if previous_value is None else value - previous_value
-        if during_event == 0 and event_conductance_min <= change and event_conductance_max >= change:
+        if (
+            during_event == 0
+            and event_conductance_min <= change
+            and event_conductance_max >= change
+        ):
             print(f"Starting event at time {time}")
             during_event = 1
         elif during_event == 1 and change < 0:
             print(f"Ending event at time {time}")
             during_event = 0
-        new_csv.append((time, during_event))
+        forwards_csv.append((time, during_event))
         previous_value = value
 
-    new_csv = pd.DataFrame(new_csv, columns=["time", "event"])
-    new_csv.to_csv(csv_path.parent / f"{csv_path.name[:-4]}_events.csv")
+    forwards_csv = pd.DataFrame(forwards_csv, columns=["time", "event"])
+    forwards_csv.to_csv(csv_path.parent / f"{csv_path.name[:-4]}_events_lr.csv")
+
+    during_event = 0
+    previous_value = None
+    reverse_csv = []
+    for i, (indx, time, value) in list(csv.iterrows()[::-1]):
+        change = 0 if previous_value is None else value - previous_value
+        if (
+            during_event == 0
+            and event_conductance_min <= change
+            and event_conductance_max >= change
+        ):
+            print(f"Starting event at time {time}")
+            during_event = 1
+        elif during_event == 1 and change < 0:
+            print(f"Ending event at time {time}")
+            during_event = 0
+        reverse_csv.append((time, during_event))
+        previous_value = value
+    reverse_csv = pd.DataFrame(reverse_csv[::-1], columns=["time", "event"])
+    reverse_csv.to_csv(csv_path.parent / f"{csv_path.name[:-4]}_events_rl.csv")
 
 
 if __name__ == "__main__":
