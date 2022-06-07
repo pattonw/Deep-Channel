590e4737de1cbed7c5ccdb388af3a47ad7b068c6
pattonw <pattonw@hhmi.org>
2022-06-07 14:50:04 -0400

    Add option for defining a threshold

--- extract_multiple_by_mean.py
+++ extract_multiple_by_mean.py
@@ -8,19 +8,24 @@ import click
 @click.option(
     "--csv", type=click.Path(exists=True), help="The csv from which to extract events."
 )
-def convert_mean(csv):
+@click.option("-t", "--threshold", type=float, default=None)
+def convert_mean(csv, threshold):
     csv_path = Path(csv)
 
     csv = Path(csv)
 
     csv = pd.read_csv(csv)
 
-    threshold = csv["conductancepS"].mean()
+    if threshold is None:
+        threshold = csv["conductancepS"].mean()
     thresholded = (csv["conductancepS"] > threshold).astype(int)
 
-    new_csv = pd.DataFrame(list(zip(csv["time"], thresholded)), columns=["time", "event"])
+    new_csv = pd.DataFrame(
+        list(zip(csv["time"], thresholded)), columns=["time", "event"]
+    )
     new_csv.to_csv(csv_path.parent / f"{csv_path.name[:-4]}_events.csv")
 
+    print(new_csv)
 
 if __name__ == "__main__":
     convert_mean()
